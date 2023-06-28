245299116b4f0e18307f9624ef01602aafc2c9dc
Pratim Ghosh <pratimghosh90@gmail.com>
2023-06-28 23:09:00 +0530

    grambahar- why choose section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <HomeProduct/>
     <HeroBottom/>
     {/* this section contains the image  */}
-      <div className="grambahar-cartoon-container">
+      {/* <div className="grambahar-cartoon-container">
         <img src="images/grambahar-cartoon.jpg" alt="a" className="grambahar-cartoon"/>
         <div className="grambahar-home-page-title">
               Gram<span id="grambahar-home-bahar">Bahar</span> - "The taste of 90's"  <br />
@@ -17,8 +17,29 @@ const HomePage = () => {
               </span>
         </div>
 
-      </div>
+      </div> */}
       {/* the image sections ends here  */}
+      <div className="home-page-container">
+        <div className="grambahar-product-story-container">
+          <div className="grambahar-farmer-image">
+          <img src="images/grambahar-cartoon.jpg" alt="a" className="grambahar-cartoon"/>
+          </div>
+          <div className="grambahar-product-story">
+            <div className="story-title">
+              Why Choose GramBahar?
+            </div>
+            <div className="line">
+              
+            </div>
+            <div className="story">
+            Grambahar.com brings you ORGANIC date palm jaggery, loaded with essential nutrients. It is high in iron which helps in treating anemia by increasing the hemoglobin level. It also contains magnesium, calcium, phosphorus, and potassium.
+Our local farmers climb tall date palm trees, tap it for its fresh sap and hang their earthen pots into which the sap (neera) trickles all night long. The pots are removed during early dawn hours, filtered through a layered seive and then boiled for many hours at controlled temperature in tin or steel flat vessels fixed on firewood pits - also called a mud oven.
+
+This product contains no additives, adulterated sugar, artificial preservatives, synthetic fillers or binders. 100% natural and 100% ORGANIC.
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
