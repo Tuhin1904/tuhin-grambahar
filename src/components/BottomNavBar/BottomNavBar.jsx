427511161de388ffe2f7bce6ce656a76178d9ef4
Pratim Ghosh <pratimghosh90@gmail.com>
2023-07-22 14:32:18 +0530

    bottom-navbar

--- src/components/BottomNavBar/BottomNavBar.jsx
+++ src/components/BottomNavBar/BottomNavBar.jsx
@@ -5,16 +5,22 @@ import FolderIcon from '@mui/icons-material/Folder';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import StarsOutlinedIcon from '@mui/icons-material/StarsOutlined';
 import Paper from '@mui/material/Paper';
 import './BottomNavBar.css'
-import { ThemeProvider, createTheme } from '@mui/material';
+import { Rating, ThemeProvider, createTheme } from '@mui/material';
 const theme  = createTheme({
     palette:{
         primary:{
             main:"#7ab35b"
         },
         secondary:{
-            main:'#7ab35b'
+            main:'#ffbb51'
         }
     }
 })
@@ -28,24 +34,34 @@ export default function LabelBottomNavigation() {
   
   return (
     <ThemeProvider theme={theme}>
-    <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={5} className={'bottom-nav'}>
+    <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={5} className={'bottom-nav'} >
     <BottomNavigation  value={value} onChange={handleChange}>
       <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
+        label="Rewards"
+        value="Rewards"
+        icon={<EmojiEventsOutlinedIcon color="primary"/>}
+      />
+       <BottomNavigationAction
+        label="Review"
+        value="Review"
+        icon={<StarsOutlinedIcon color="primary"/>}
+      />
+       <BottomNavigationAction
+        label="Home"
+        value="Home"
+        icon={<HomeOutlinedIcon color="primary" fontSize="large"/>}
       />
       <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
+        label="Add"
+        value="Add"
+        icon={<ControlPointIcon color="primary"/>}
       />
       <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
+        label="Cart"
+        value="Cart"
+        icon={<AddShoppingCartOutlinedIcon color="primary"/>}
       />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+      
     </BottomNavigation>
     </Paper>
     </ThemeProvider>
