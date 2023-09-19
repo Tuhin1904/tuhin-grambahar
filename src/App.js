7dfb75be73933f9cdaf439c569bf097452812495
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-09-19 21:13:50 +0530

    show error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import HomePage from './pages/HomePage/HomePage'
+import HomePage from './pages/HomePage/HomePage';
 import TopHeader from './components/TopHeaderOffer/TopHeader';
 import NavBar from './components/NavBar/NavBar';
-const App =()=> {
+const App = () => {
   return (
-   <>
-     <TopHeader/>
+    <>
+      {/* <TopHeader/>
       <NavBar/>
-      <HomePage/>
-   </>
+      <HomePage/> */}
+
+      <div className="flex flex-col items-center justify-center h-screen text-4xl font-bold text-center text-white bg-red-600">
+        <div class="container">
+          <h1 className="mb-2">Error 403</h1>
+          <p>Access not granted, server error!</p>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
