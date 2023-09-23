ef26820a956b3c32ddb2983eeb544c6797b56b1f
Pratim Ghosh <pratimghosh90@gmail.com>
2023-09-23 23:47:38 +0530

    description

--- src/components/HomeProdDescription/HomeProductDescription.jsx
+++ src/components/HomeProdDescription/HomeProductDescription.jsx
@@ -5,6 +5,9 @@ const HomeProductDescription = () => {
         <div className={classes.heading}>
         DESCRIPTION
         </div>
+        <div className={classes.line}>
+
+        </div>
         <div className={classes.description}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi consequatur cupiditate aliquam suscipit ut iusto.
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sit impedit obcaecati illum mollitia iusto!
