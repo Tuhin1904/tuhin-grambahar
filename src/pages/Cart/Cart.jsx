45d131e78a73e5ee46417e7922b61a510f296af9
Pratim Ghosh <pratimghosh90@gmail.com>
2023-09-25 02:06:18 +0530

    cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import classes from './Cart.module.css'
+import CartItems from '../../components/Cart/CartItems'
+import { Icon } from '@iconify/react';
 const Cart = () => {
+  const subTotal = 500;
   return (
-    <div>Cart</div>
+    <div className={classes.container}>
+      <div className={classes.message}>
+        MESSAGES ABOUT ITEMS IN YOUR CART
+      </div>
+      <div className={classes.subtotal}>
+       Subtotal is = <span className={classes.subtotal_value}> {subTotal} . <sup style={{fontSize:"0.8rem"}}>00</sup> /-</span>  INR
+      </div>
+      <div className={classes.free_delivery}> <Icon icon="mdi:tick-decagram" style={{fontSize:"1.6rem",marginRight:"10px",color:"#edb911"}}/> Congratulations Your Order is elegible for free delivery</div>
+
+      <div className={classes.proceed_btn}>Proceed to checkout</div>
+    {/* <CartItems/> */}
+
+    </div>
   )
 }
 
-export default Cart
+export default Cart;
