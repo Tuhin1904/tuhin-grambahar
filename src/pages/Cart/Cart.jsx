e673fc91c3f2ef6bde6fad0cf04029800b45ac4e
Pratim Ghosh <pratimghosh90@gmail.com>
2023-09-26 02:37:49 +0530

    cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
         MESSAGES ABOUT ITEMS IN YOUR CART
       </div>
       <div className={classes.subtotal}>
-       Subtotal is = <span className={classes.subtotal_value}> {subTotal} . <sup style={{fontSize:"0.8rem"}}>00</sup> /-</span>  INR
+       Subtotal is = <span className={classes.subtotal_value}> {subTotal} <sup style={{fontSize:"0.8rem"}}>00</sup> /-</span>  INR
       </div>
       <div className={classes.free_delivery}> <Icon icon="mdi:tick-decagram" style={{fontSize:"1.6rem",marginRight:"10px",color:"#edb911"}}/> Congratulations Your Order is elegible for free delivery</div>
 
