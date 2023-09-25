a5d909cf083f910d3af7637d3e9bf8178d6e7dba
Pratim Ghosh <pratimghosh90@gmail.com>
2023-09-26 02:32:20 +0530

    cart v3

--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -9,6 +9,9 @@ const CartItems = () => {
           <div className={classes.prod_image}>
             <img src={'./images/product-transparent.png'} alt="" />
           </div>
+          <div className={classes.btn}>
+            
+          </div>
         </div>
         {/* this is the partision between two sections  */}
         <div className={classes.cart_item_right}>
@@ -17,25 +20,25 @@ const CartItems = () => {
             <div className={classes.box}>30% OFF</div>
             <div className={classes.offer}>Limited time deal</div>
           </div>
+          {/* price box starts here */}
           <div className={classes.price_box}>
             <div className={classes.price}>
-              <span style={{display:"inline"}}>
-                <Icon icon="fa:rupee"  style={{fontSize:"15px",marginTop:"5px",color:"brown"}}/>
+              <span style={{ display: 'inline' }}>
+                <Icon icon="fa:rupee" style={{ fontSize: '15px', marginTop: '5px', color: 'brown' }} />
               </span>
               299
-              <span style={{fontSize:"15px",marginTop:"1px",color:"brown"}}>
-                00
-              </span>
-              <span className={classes.mrp}>
-                M.R.P
-              </span>
-              <strike className={classes.mrp_price}>
-              <Icon icon="fa:rupee" style={{fontSize:"1rem",marginTop:"1px",color:"rgb(30, 31, 31)",fontWeight:"100"}}/>
-                500.00
-               
+              <span style={{ fontSize: '15px', marginTop: '1px', color: 'brown' }}>00</span>
+              <span className={classes.mrp}>M.R.P. :</span>
+              <strike style={{ color: 'brown' }}>
+                {/* <Icon icon="fa:rupee" style={{fontSize:"1rem",marginTop:"1px",color:"rgb(30, 31, 31)",fontWeight:"100"}}/> */}
+
+                <span className={classes.mrp_price}> 500.00</span>
               </strike>
+              <span className={classes.mrp_price}>INR</span>
             </div>
           </div>
+          {/* price box ends here  */}
+          <div className={classes.free_delivery}>You are eigible for free delivery</div>
         </div>
       </div>
       <hr />
