cd49c6b8cb90817bcca510760099667742916b88
Pratim Ghosh <pratimghosh90@gmail.com>
2023-09-26 02:21:59 +0530

    color

--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Cartitems.module.css';
+import { Icon } from '@iconify/react';
 const CartItems = () => {
   return (
     <>
@@ -13,15 +14,27 @@ const CartItems = () => {
         <div className={classes.cart_item_right}>
           <div className={classes.title}>Date Palm Jaggery</div>
           <div className={classes.offer_box}>
-            <div className={classes.box}>
-              30% OFF
-            </div>
-            <div className={classes.offer}>
-              Limited time deal
-            </div>
+            <div className={classes.box}>30% OFF</div>
+            <div className={classes.offer}>Limited time deal</div>
           </div>
           <div className={classes.price_box}>
-            <div className={classes.price}>299</div>
+            <div className={classes.price}>
+              <span style={{display:"inline"}}>
+                <Icon icon="fa:rupee"  style={{fontSize:"15px",marginTop:"5px",color:"brown"}}/>
+              </span>
+              299
+              <span style={{fontSize:"15px",marginTop:"1px",color:"brown"}}>
+                00
+              </span>
+              <span className={classes.mrp}>
+                M.R.P
+              </span>
+              <strike className={classes.mrp_price}>
+              <Icon icon="fa:rupee" style={{fontSize:"1rem",marginTop:"1px",color:"rgb(30, 31, 31)",fontWeight:"100"}}/>
+                500.00
+               
+              </strike>
+            </div>
           </div>
         </div>
       </div>
