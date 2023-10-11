e358fdafedc706ef951bcea0995e06db85b7c83f
Pratim Ghosh <pratimghosh90@gmail.com>
2023-10-11 23:14:46 +0530

    problem

---
+++ src/components/SecondaryBottomNavBar/SecondaryBottomNavbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from './SecondaryBottomNavBar.module.css';
+const SecondaryBottomNavbar = () => {
+  return <div className={classes.container}>SecondaryBottomNavbar</div>;
+};
+
+export default SecondaryBottomNavbar;
