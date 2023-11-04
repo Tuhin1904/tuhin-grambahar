50440c4b1955ecacfb4abb8cd8f7ac8d7da33c2c
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-11-04 23:12:09 +0530

    Add headers to axios request with access token

--- src/services/auth.helper.js
+++ src/services/auth.helper.js
@@ -6,7 +6,9 @@ export const getAxiosHeaderWithAccessToken = () => {
   const user = localStorage.getItem('user');
   const token = user ? JSON.parse(user).auth : '';
   return {
-    ...DEFAULT_AXIOS_HEADER,
-    Authorization: `Bearer ${token}`,
+    headers: {
+      ...DEFAULT_AXIOS_HEADER,
+      Authorization: `Bearer ${token}`,
+    },
   };
 };
