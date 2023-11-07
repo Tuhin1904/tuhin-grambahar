b66d16543cf64fb74851c249bcadbc7ed83c80a8
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-11-08 00:15:59 +0530

    Add cart manipulation functions to cart.services.js

--- src/services/cart.servies.js
+++ src/services/cart.servies.js
@@ -17,3 +17,50 @@ export const updateMyCart = async (products = []) => {
   );
   return res.data;
 };
+
+export const addToCart = async (productId, quantity) => {
+  const cart = await getMyCart();
+  const products = cart.products || [];
+  const product = products.find((item) => item.product._id === productId);
+  if (product) {
+    product.quantity += quantity;
+  } else {
+    products.push({
+      product: productId,
+      quantity,
+    });
+  }
+  const res = await updateMyCart(products);
+  return res;
+};
+
+export const removeFromCart = async (productId) => {
+  const cart = await getMyCart();
+  const products = cart.products || [];
+  const newProducts = products.filter((item) => item.product._id !== productId);
+  const res = await updateMyCart(newProducts);
+  return res;
+};
+
+export const updateQuantity = async (productId, quantity) => {
+  const cart = await getMyCart();
+  const products = cart.products || [];
+  const product = products.find((item) => item.product._id === productId);
+  if (product) {
+    product.quantity = quantity;
+  }
+  const res = await updateMyCart(products);
+  return res;
+};
+
+export const removeFromCartProductQuantity = async (productId, quantity) => {
+  const cart = await getMyCart();
+  const products = cart.products || [];
+  const product = products.find((item) => item.product._id === productId);
+  if (product) {
+    product.quantity -= quantity;
+    product.quantity = Math.max(0, product.quantity);
+  }
+  const res = await updateMyCart(products);
+  return res;
+};
