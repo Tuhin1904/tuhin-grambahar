d66ce34a5dcd26ff2bae4e09585379afc2b89fd3
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-11-10 04:42:54 +0530

    Add address editing functionality to AddressScreen component

---
+++ src/components/OderProcessingDialog/AddressModificationDialog.js
@@ -0,0 +1,189 @@
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { forwardRef, useState } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import stateData from '@/data/state.json';
+import FullWithPrimaryButton from './FullWithPrimaryButton';
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function AddressModificationDialog({
+  open,
+  title,
+  initialAddress,
+  children,
+  actionButtonLabel = '',
+  actionButtonHandler,
+  onClickCLoseHandler,
+  loading = false,
+}) {
+  const isMobile = useMediaQuery('(max-width:639px)');
+  const [addressDetails, setAddressDetails] = useState({
+    name: initialAddress?.name || '',
+    address_line_1: initialAddress?.address_line_1 || '',
+    address_line_2: initialAddress?.address_line_2 || '',
+    district: initialAddress?.district || '',
+    state: initialAddress?.state || '',
+    pin: initialAddress?.pin || '',
+    country: 'India',
+    phone_number: initialAddress?.phone_number || '',
+    landmark: initialAddress?.landmark || '',
+  });
+
+  return (
+    <Dialog open={open} TransitionComponent={Transition} keepMounted maxWidth="sm" fullWidth fullScreen={isMobile}>
+      <DialogTitle>
+        <button type="button" className="mr-3 text-secondary-black" onClick={onClickCLoseHandler}>
+          <ArrowBackIcon sx={{ fontSize: '20px' }} />
+        </button>
+        {title}
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          label="Name"
+          type="text"
+          required
+          variant="standard"
+          placeholder="Riya"
+          value={addressDetails.name}
+          sx={{ mb: 3 }}
+          onChange={(event) => setAddressDetails((current) => ({ ...current, name: event.target.value }))}
+        />
+
+        <TextField
+          fullWidth
+          label="Address Line 1"
+          type="text"
+          required
+          variant="standard"
+          placeholder="House No. 123, Street No. 123, Area"
+          value={addressDetails.address_line_1}
+          sx={{ mb: 3 }}
+          onChange={(event) => setAddressDetails((current) => ({ ...current, address_line_1: event.target.value }))}
+        />
+
+        <TextField
+          fullWidth
+          label="Address Line 2"
+          type="text"
+          // required
+          variant="standard"
+          placeholder="Locality"
+          value={addressDetails.address_line_2}
+          sx={{ mb: 3 }}
+          onChange={(event) => setAddressDetails((current) => ({ ...current, address_line_2: event.target.value }))}
+        />
+
+        <TextField
+          fullWidth
+          label="Landmark"
+          type="text"
+          required
+          variant="standard"
+          placeholder="Near XYZ"
+          value={addressDetails.landmark}
+          sx={{ mb: 3 }}
+          onChange={(event) => setAddressDetails((current) => ({ ...current, landmark: event.target.value }))}
+        />
+
+        <TextField
+          fullWidth
+          label="Phone Number"
+          type="tel"
+          required
+          variant="standard"
+          placeholder="9876543210"
+          value={addressDetails.phone_number}
+          sx={{ mb: 3 }}
+          onChange={(event) => setAddressDetails((current) => ({ ...current, phone_number: event.target.value }))}
+        />
+
+        <div className="grid grid-cols-2 gap-x-7">
+          <FormControl
+            required
+            variant="standard"
+            fullWidth
+            sx={{
+              mb: 3,
+            }}
+          >
+            <InputLabel>State</InputLabel>
+            <Select
+              required
+              label="State"
+              variant="standard"
+              value={addressDetails.state}
+              onChange={(event) => setAddressDetails((current) => ({ ...current, state: event.target.value }))}
+            >
+              {stateData?.states?.map((state) => (
+                <MenuItem key={state.id} value={state.name}>
+                  {state.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <TextField
+            fullWidth
+            label="District"
+            type="text"
+            required
+            variant="standard"
+            placeholder="District"
+            value={addressDetails.district}
+            sx={{ mb: 3 }}
+            onChange={(event) => setAddressDetails((current) => ({ ...current, district: event.target.value }))}
+          />
+        </div>
+
+        <div className="grid grid-cols-2 gap-x-7">
+          <TextField
+            fullWidth
+            label="PIN"
+            type="number"
+            required
+            variant="standard"
+            placeholder="789456"
+            value={addressDetails.pin}
+            sx={{ mb: 3 }}
+            onChange={(event) => setAddressDetails((current) => ({ ...current, pin: event.target.value }))}
+          />
+
+          <TextField
+            fullWidth
+            label="Country"
+            type="text"
+            required
+            disabled
+            variant="standard"
+            placeholder="India"
+            value={addressDetails.country}
+            sx={{ mb: 3 }}
+            onChange={(event) => setAddressDetails((current) => ({ ...current, country: event.target.value }))}
+          />
+        </div>
+
+        {children}
+
+        <div className="mt-6">
+          <FullWithPrimaryButton loading={loading} onClick={() => actionButtonHandler(addressDetails)}>
+            {actionButtonLabel}
+          </FullWithPrimaryButton>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddressModificationDialog;
--- src/components/OderProcessingDialog/AddressScreen.js
+++ src/components/OderProcessingDialog/AddressScreen.js
@@ -1,11 +1,157 @@
+import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import ErrorAlert from './ErrorAlert';
+import { updateMyAddress } from '@/services/account.services';
+import AddressModificationDialog from './AddressModificationDialog';
 import OrderProductDetails from './OrderProductDetails';
 
 function AddressScreen({ product, quantity, userAddress, setUserAddress }) {
+  console.log('📢[AddressScreen.js:4]: userAddress: ', userAddress);
+  const [openAddressEditingDialog, setOpenAddressEditingDialog] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [deleteAddressId, setDeleteAddressId] = useState('');
+
+  const onAddressUpdateHandler = async (address) => {
+    setLoading(true);
+    setError('');
+    try {
+      const response = await updateMyAddress({
+        id: openAddressEditingDialog,
+        name: address.name,
+        addressLine1: address.address_line_1,
+        addressLine2: address.address_line_2,
+        phoneNumber: address.phone_number,
+        country: address.country,
+        state: address.state,
+        district: address.district,
+        pin: address.pin,
+        landmark: address.landmark,
+      });
+
+      setUserAddress((prev) => {
+        return prev.map((addr) => {
+          if (addr.id === openAddressEditingDialog) {
+            return {
+              ...addr,
+              address_line_1: response?.address_line_1 || '',
+              address_line_2: response?.address_line_2 || '',
+              country: response?.country || '',
+              district: response?.district || '',
+              landmark: response?.landmark || '',
+              name: response?.name || '',
+              phone_number: response?.phone_number || '',
+              pin: response?.pin || '',
+              state: response?.state || '',
+            };
+          }
+          return addr;
+        });
+      });
+      setOpenAddressEditingDialog(() => null);
+    } catch (err) {
+      console.error('📢[AddressScreen.js]: err: ', err);
+      setError(err?.response?.data?.error || err?.message || 'Error at updating address');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="py-3 border-b shadow-sm mb-9 border-primary">
         <OrderProductDetails product={product} quantity={quantity} readOnly />
       </div>
+
+      {userAddress?.length > 0 && (
+        <ul className="text-sm text-primary-black">
+          {userAddress.map((address) => (
+            <li key={address.id} className="pb-2 mb-2 border-b last:border-b-0 border-x-gray-800">
+              <div className="mb-0.5 flex">
+                <p className="flex-grow mr-3 font-medium">{address.name}</p>
+                <button
+                  type="button"
+                  className="text-secondary-black"
+                  onClick={() => setOpenAddressEditingDialog(() => address.id)}
+                >
+                  <EditIcon
+                    sx={{
+                      fontSize: '18px',
+                    }}
+                  />
+                </button>
+              </div>
+              <p className="mb-0.5">{address.address_line_1}</p>
+              <p className="mb-0.5">
+                {address.address_line_2}, {address.landmark}
+              </p>
+              <p className="">
+                {address.district}, {address.state} - {address.pin}, {address.country}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {openAddressEditingDialog && (
+        <AddressModificationDialog
+          open={openAddressEditingDialog}
+          title="Edit Address"
+          initialAddress={userAddress.find((address) => address.id === openAddressEditingDialog)}
+          actionButtonLabel={
+            <>
+              <CheckIcon sx={{ mr: 1.5, fontSize: '22px' }} />
+              Update Address
+            </>
+          }
+          onClickCLoseHandler={() => setOpenAddressEditingDialog(() => null)}
+          loading={loading}
+          actionButtonHandler={onAddressUpdateHandler}
+        >
+          <div className="flex items-end justify-end my-3">
+            <button
+              type="button"
+              onClick={() => setDeleteAddressId(() => openAddressEditingDialog)}
+              className="flex items-center text-sm font-medium text-red-500"
+            >
+              <DeleteIcon sx={{ fontSize: '20px', mr: 1 }} />
+              Delete Address
+            </button>
+
+            <Dialog
+              open={deleteAddressId}
+              onClose={() => setDeleteAddressId('')}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">Delete Address</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Are you sure you want to delete this address?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button disabled={loading} color="inherit" onClick={() => setDeleteAddressId('')}>
+                  Cancel
+                </Button>
+                <Button disabled={loading} autoFocus color="error">
+                  Yes, Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+        </AddressModificationDialog>
+      )}
+
+      <ErrorAlert error={error} />
     </>
   );
 }
