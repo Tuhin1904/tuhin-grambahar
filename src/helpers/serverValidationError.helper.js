0eedcc5a0f83e4ec95dc1c8f3e397bfe701923de
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-11-14 03:11:42 +0530

    Refactor serverValidationError.helper.js to handle string errors

--- src/helpers/serverValidationError.helper.js
+++ src/helpers/serverValidationError.helper.js
@@ -14,11 +14,17 @@ export const humanityText = (text) => {
 
 export const extractServerValidationError = (error) => {
   let errorMessage = '';
-  if (error?.response?.data?.errors) {
-    const errors = error?.response?.data?.errors || [];
-    if (errors.length) {
-      errorMessage = errors.map((err) => `Error: ${humanityText(err.path)} --> ${err.msg}`).join(' ');
+  try {
+    if (error?.response?.data?.errors) {
+      const errors = error?.response?.data?.errors || [];
+      if (typeof errors === 'string') {
+        errorMessage = errors;
+      } else if (errors.length) {
+        errorMessage = errors.map((err) => `Error: ${humanityText(err.path)} --> ${err.msg}`).join(' ');
+      }
     }
+  } catch {
+    errorMessage = '';
   }
 
   return errorMessage;
