2bbd83636d6b7360906ef3848992b93ade48b990
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2023-11-16 03:37:13 +0530

    Refactor code for improved readability and performance.

---
+++ src/pages/my-orders/[order].js
@@ -0,0 +1,9 @@
+function MyOrderPage({ data }) {
+  return (
+    <div>
+      <h1>My Order Page</h1>
+    </div>
+  );
+}
+
+export default MyOrderPage;
---
+++ src/pages/my-orders/index.js
@@ -0,0 +1,9 @@
+function MyOrdersPage() {
+  return (
+    <div>
+      <h1>My Orders Page</h1>
+    </div>
+  );
+}
+
+export default MyOrdersPage;
